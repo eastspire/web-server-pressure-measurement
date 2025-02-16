d7212bdd74f9db67409645d112a4a7efcd3e045d
尤雨东 <root@ltpp.vip>
2025-02-16 11:52:50 +0800

    feat: dependencies

--- hyperlane/src/main.rs
+++ hyperlane/src/main.rs
@@ -1,16 +1,7 @@
 use hyperlane::*;
 
 fn test_sync_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
-    let controller_data: RwLockWriteControllerData = arc_lock_controller_data.write().unwrap();
-    let mut response: Response = controller_data.get_response().clone();
-    let stream: ArcTcpStream = controller_data.get_stream().clone().unwrap();
-    response
-        .set_body("hello".into())
-        .set_status_code(200)
-        .set_header(CONTENT_TYPE, APPLICATION_JSON)
-        .set_header(CONTENT_ENCODING, CONTENT_ENCODING_GZIP)
-        .send(&stream)
-        .unwrap();
+    let _ = send_response(&arc_lock_controller_data, 200, "hello");
 }
 
 fn run_server() {
@@ -18,6 +9,7 @@ fn run_server() {
     server.host("0.0.0.0");
     server.port(60000);
     server.log_dir("./logs");
+    server.log_interval_millis(1_000_000_000);
     server.middleware(test_sync_middleware);
     server.listen();
 }
