aa55304f88cb932f2d7f8239fad089635a6c3ccb
尤雨东 <root@ltpp.vip>
2025-02-19 08:37:07 +0800

    feat: dependencies

--- close-keep-alive/hyperlane/src/main.rs
+++ close-keep-alive/hyperlane/src/main.rs
@@ -1,20 +1,20 @@
 use hyperlane::*;
 
-fn test_sync_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
-    let _ = send_response(&arc_lock_controller_data, 200, "hello");
+async fn test_sync_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
+    let _ = send_response(&arc_lock_controller_data, 200, "hello").await;
 }
 
-fn run_server() {
+async fn run_server() {
     let mut server: Server = Server::new();
-    server.host("0.0.0.0");
-    server.port(60000);
-    server.log_dir("./logs");
-    server.log_interval_millis(1_000_000_000);
-    server.middleware(test_sync_middleware);
-    server.listen();
+    server.host("0.0.0.0").await;
+    server.port(60000).await;
+    server.log_dir("./logs").await;
+    server.log_interval_millis(1_000_000_000).await;
+    server.middleware(test_sync_middleware).await;
+    server.listen().await;
 }
 
 #[tokio::main]
 async fn main() {
-    run_server();
+    run_server().await;
 }
--- open-keep-alive/hyperlane/src/main.rs
+++ open-keep-alive/hyperlane/src/main.rs
@@ -1,28 +1,32 @@
 use hyperlane::*;
 
-fn test_sync_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
-    let controller_data: ControllerData = get_controller_data(&arc_lock_controller_data);
+async fn test_sync_middleware(arc_lock_controller_data: ArcRwLockControllerData) {
+    let controller_data: ControllerData = get_controller_data(&arc_lock_controller_data).await;
     let mut response: Response = controller_data.get_response().clone();
     let body: &str = "hello";
-    let stream: ArcTcpStream = controller_data.get_stream().clone().unwrap();
+    let stream_opt: OptionArcRwLockStream = get_stream(&arc_lock_controller_data).await;
+    if stream_opt.is_none() {
+        return;
+    }
     let _: ResponseResult = response
         .set_body(body)
         .set_status_code(200)
         .set_header(CONNECTION, CONNECTION_KEEP_ALIVE)
-        .send(&stream);
+        .send(&stream_opt.unwrap())
+        .await;
 }
 
-fn run_server() {
+async fn run_server() {
     let mut server: Server = Server::new();
-    server.host("0.0.0.0");
-    server.port(60000);
-    server.log_dir("./logs");
-    server.log_interval_millis(1_000_000_000);
-    server.middleware(test_sync_middleware);
-    server.listen();
+    server.host("0.0.0.0").await;
+    server.port(60000).await;
+    server.log_dir("./logs").await;
+    server.log_interval_millis(1_000_000_000).await;
+    server.middleware(test_sync_middleware).await;
+    server.listen().await;
 }
 
 #[tokio::main]
 async fn main() {
-    run_server();
+    run_server().await;
 }
