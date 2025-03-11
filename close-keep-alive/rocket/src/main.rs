1cf229f4dbc1073888330ae70e976cb85066be3e
尤雨东 <root@ltpp.vip>
2025-03-11 22:21:12 +0800

    feat: rocket

--- close-keep-alive/rocket/src/main.rs
+++ close-keep-alive/rocket/src/main.rs
@@ -1,15 +1,33 @@
 #[macro_use]
 extern crate rocket;
+use rocket::{fairing::{Fairing, Info, Kind}, http::Header, Request, Response};
+
+struct DisableKeepAlive;
+
+#[rocket::async_trait]
+impl Fairing for DisableKeepAlive {
+    fn info(&self) -> Info {
+        Info {
+            name: "Disable Keep-Alive",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, _: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Connection", "close"));
+    }
+}
 
 #[get("/")]
-async fn index() -> String {
-    "hello".to_string()
+async fn index() -> &'static str {
+    "hello"
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
+        .attach(DisableKeepAlive)
         .configure(rocket::Config {
             log_level: rocket::config::LogLevel::Off,
             port: 60000,
