8695ec1a39c2ac11b6e1da535f41197fca43ecf5
尤雨东 <root@ltpp.vip>
2025-03-23 07:39:38 +0800

    feat: hyperlane

--- close-keep-alive/hyperlane/src/main.rs
+++ close-keep-alive/hyperlane/src/main.rs
@@ -9,14 +9,14 @@ async fn test_sync_middleware(controller_data: ControllerData) {
 }
 
 async fn run_server() {
-    let mut server: Server = Server::new();
-    server.host("0.0.0.0");
-    server.port(60000);
-    server.log_dir("./logs");
-    server.disable_inner_log();
-    server.disable_inner_print();
-    server.log_interval_millis(1_000_000_000);
-    server.request_middleware(test_sync_middleware);
+    let mut server: Server = Server::new().await;
+    server.host("0.0.0.0").await;
+    server.port(60000).await;
+    server.log_dir("./logs").await;
+    server.disable_inner_log().await;
+    server.disable_inner_print().await;
+    server.log_interval_millis(1_000_000_000).await;
+    server.request_middleware(test_sync_middleware).await;
     server.listen().await;
 }
 
