d8f89fd800029134c6ab5852e4d883d6157d488c
尤雨东 <root@ltpp.vip>
2025-08-25 22:00:53 +0800

    feat: 2025-08-25 22:00:53

--- sh/update-lock.sh
+++ sh/update-lock.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd ./open-keep-alive/hyperlane;
 rm -rf Cargo.lock;
+cargo fmt;
 cargo check;
 cd ../../close-keep-alive/hyperlane;
 rm -rf Cargo.lock;
+cargo fmt;
 cargo check;
 cd ../../test-request;
 rm -rf Cargo.lock;
+cargo fmt;
 cargo check;
