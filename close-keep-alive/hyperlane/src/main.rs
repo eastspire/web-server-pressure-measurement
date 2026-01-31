bf39663daed344cd72bcae9216aa487cafa16008
尤雨东 <root@ltpp.vip>
2026-01-31 20:05:42 +0800

    feat: 2026-01-31 20:05:42

--- close-keep-alive/hyperlane/src/main.rs
+++ close-keep-alive/hyperlane/src/main.rs
@@ -37,34 +37,7 @@ impl ServerHook for RootRoute {
 
 #[tokio::main]
 async fn main() {
-    let request_config: RequestConfig = RequestConfig::new().await;
-    request_config
-        .buffer_size(KB_4)
-        .await
-        .http_read_timeout_ms(u64::MAX)
-        .await
-        .max_body_size(usize::MAX)
-        .await
-        .max_header_count(usize::MAX)
-        .await
-        .max_header_key_length(usize::MAX)
-        .await
-        .max_header_line_length(usize::MAX)
-        .await
-        .max_header_value_length(usize::MAX)
-        .await
-        .max_path_length(usize::MAX)
-        .await
-        .max_query_length(usize::MAX)
-        .await
-        .max_request_line_length(usize::MAX)
-        .await
-        .max_ws_frame_size(usize::MAX)
-        .await
-        .max_ws_frames(usize::MAX)
-        .await
-        .ws_read_timeout_ms(u64::MAX)
-        .await;
+    let request_config: RequestConfig = RequestConfig::low_security().await;
     let server_config: ServerConfig = ServerConfig::new().await;
     server_config.port(60000).await.disable_nodelay().await;
     Server::new()
