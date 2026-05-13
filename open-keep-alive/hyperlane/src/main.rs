d5159e85c6dde6dc969cda7a9fe40ee9f7befa3b
sheng <root@ltpp.vip>
2026-05-13 08:35:59 +0800

    feat: 2026-05-13 08:35:59

--- open-keep-alive/hyperlane/src/main.rs
+++ open-keep-alive/hyperlane/src/main.rs
@@ -2,60 +2,61 @@ use hyperlane::*;
 
 pub const BODY: &[u8] = b"Hello";
 
-struct RootRoute;
 struct TaskPanicHook;
 
 impl ServerHook for TaskPanicHook {
-    async fn new(_ctx: &mut Context) -> Self {
+    async fn new(_: &mut Stream, _: &mut Context) -> Self {
         Self
     }
 
-    async fn handle(self, _ctx: &mut Context) {}
+    async fn handle(self, stream: &mut Stream, ctx: &mut Context) -> Status {
+        let data: Vec<u8> = ctx
+            .get_mut_response()
+            .set_version(HttpVersion::Http1_1)
+            .set_status_code(500)
+            .set_body("Internal Server Error")
+            .build();
+        if stream.try_send(data).await.is_err() {
+            stream.set_closed(true);
+            return Status::Reject;
+        }
+        Status::Continue
+    }
 }
 
+struct RootRoute;
+
 impl ServerHook for RootRoute {
-    async fn new(_ctx: &mut Context) -> Self {
+    async fn new(_: &mut Stream, _: &mut Context) -> Self {
         Self
     }
 
-    async fn handle(self, ctx: &mut Context) {
-        let _ = ctx
+    async fn handle(self, stream: &mut Stream, ctx: &mut Context) -> Status {
+        let data: Vec<u8> = ctx
             .get_mut_response()
             .set_version(HttpVersion::Http1_1)
             .set_header(CONNECTION, KEEP_ALIVE)
             .set_status_code(200)
-            .set_body(BODY);
-        if ctx.try_send().await.is_err() {
-            ctx.set_closed(true);
-            return;
+            .set_body(BODY)
+            .build();
+        if stream.try_send(data).await.is_err() {
+            stream.set_closed(true);
+            return Status::Reject;
         }
-        let _ = ctx.flush().await;
-        while ctx.http_from_stream().await.is_ok() {
-            if ctx.try_send().await.is_err() {
-                ctx.set_closed(true);
-                return;
-            }
-            let _ = ctx.flush().await;
-        }
-        ctx.set_closed(true);
+        Status::Continue
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let request_config: RequestConfig = RequestConfig::low_security();
+    let mut server: Server = Server::default();
     let mut server_config: ServerConfig = ServerConfig::default();
     server_config
         .set_address(Server::format_bind_address(DEFAULT_HOST, 60000))
         .set_nodelay(Some(false));
-    Server::default()
-        .server_config(server_config)
-        .request_config(request_config)
-        .task_panic::<TaskPanicHook>()
-        .route::<RootRoute>("/")
-        .run()
-        .await
-        .unwrap()
-        .wait()
-        .await;
+    server.server_config(server_config);
+    server.task_panic::<TaskPanicHook>();
+    server.route::<RootRoute>("/");
+    let server_control_hook: ServerControlHook = server.run().await.unwrap_or_default();
+    server_control_hook.wait().await;
 }
